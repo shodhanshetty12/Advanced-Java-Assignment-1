913c556c3b824252280fc92051a4e668132927cb
Shodhan kumar shetty <163324594+shodhanshetty12@users.noreply.github.com>
2025-05-20 20:37:48 +0530

    LinkedListDisplayWithPosition.java

---
+++ Exp1-ListInterface/LinkedListDisplayWithPosition.java
@@ -0,0 +1,15 @@
+//Q1c-4: Write a Java program to display elements and their positions in a linked list (using l_listobj.get(p))
+
+package linked_List_Programs_1c;
+import java.util.*;
+
+public class LinkedListDisplayWithPosition {
+ public static void main(String[] args) {
+     LinkedList<String> list = new LinkedList<>(Arrays.asList("Red", "Green", "Blue", "Yellow", "Pink"));
+
+     System.out.println("Elements with their positions:");
+     for (int i = 0; i < list.size(); i++) {
+         System.out.println("Position " + i + ": " + list.get(i));
+     }
+ }
+}
