ed8baedcff63adf942fe92826444b362e0e0326e
Shodhan kumar shetty <163324594+shodhanshetty12@users.noreply.github.com>
2025-05-22 22:14:34 +0530

    LinkedListInsertEnd.java

--- 1cLinkedlistOperations/LinkedListInsertEnd.java
+++ 1cLinkedlistOperations/LinkedListInsertEnd.java
@@ -1,5 +1,4 @@
 //Q1c-3: Write a Java program to insert the specified element at the end of a linked list (using l_listobj.offerLast("Pink"))
-
 package linked_List_Programs_1c;
 import java.util.*;
 
